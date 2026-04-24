3bc809275b5613180b970bfe10f1cee2fe6aa2cc
amshima1 <v.amshima9021@miva.edu.ng>
2026-04-24 10:22:59 +0100

    Refactor toggleMenu function
    Refactor toggleMenu function for clarity and consistency.

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ const menuBtn = document.querySelector('#mobile-menu');
 const navList = document.querySelector('#nav-list');
 const overlay = document.querySelector('#overlay');
 
-// Function to toggle menu
-const toggleMenu = () => {
+// Function to open/close menu
+function toggleMenu() {
     navList.classList.toggle('active');
-    menuBtn.classList.toggle('is-active');
     overlay.classList.toggle('active');
-};
+}
 
+// Event Listeners
 menuBtn.addEventListener('click', toggleMenu);
-overlay.addEventListener('click', toggleMenu); // Close if user clicks outside
+overlay.addEventListener('click', toggleMenu);
 
 // Close menu if a link is clicked
 document.querySelectorAll('.nav-links a').forEach(link => {
