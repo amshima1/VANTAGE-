2ebf8314a8ac15518c80d3f98b840f93715f86b3
amshima1 <v.amshima9021@miva.edu.ng>
2026-04-24 10:43:43 +0100

    Clean up comments in script.js
    Removed comments for cleaner code.

--- script.js
+++ script.js
@@ -2,17 +2,14 @@ const menuBtn = document.querySelector('#mobile-menu');
 const navList = document.querySelector('#nav-list');
 const overlay = document.querySelector('#overlay');
 
-// Function to open/close menu
 function toggleMenu() {
     navList.classList.toggle('active');
     overlay.classList.toggle('active');
 }
 
-// Event Listeners
 menuBtn.addEventListener('click', toggleMenu);
 overlay.addEventListener('click', toggleMenu);
 
-// Close menu if a link is clicked
 document.querySelectorAll('.nav-links a').forEach(link => {
     link.addEventListener('click', toggleMenu);
 });
