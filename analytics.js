779c63769a88b966de17c595906a233a0561ae6e
amshima1 <v.amshima9021@miva.edu.ng>
2026-04-28 23:55:13 +0100

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,1 +1,17 @@
+const db = firebase.firestore();
 
+db.collection("orders").onSnapshot(snapshot => {
+
+  let total = 0;
+
+  snapshot.forEach(doc => {
+    total += doc.data().total;
+  });
+
+  document.getElementById("orders").innerText =
+    "Orders: " + snapshot.size;
+
+  document.getElementById("revenue").innerText =
+    "Revenue: ₦" + total;
+
+});
