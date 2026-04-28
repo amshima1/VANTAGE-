2c9caf469f5b18cff358367fa545fda736eaa32a
amshima1 <v.amshima9021@miva.edu.ng>
2026-04-28 23:51:56 +0100

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -1,1 +1,33 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
+function getTotal(){
+  let total = 0;
+  cart.forEach(i => total += i.price);
+  return total;
+}
+
+function pay(){
+  let handler = PaystackPop.setup({
+    key: "YOUR_PAYSTACK_KEY",
+    email: "test@email.com",
+    amount: getTotal() * 100,
+
+    callback: function(response){
+      saveOrder(response.reference);
+    }
+  });
+
+  handler.openIframe();
+}
+
+function saveOrder(ref){
+  firebase.firestore().collection("orders").add({
+    cart,
+    total: getTotal(),
+    ref,
+    date: new Date()
+  });
+
+  alert("Order Successful");
+  localStorage.removeItem("cart");
+}
