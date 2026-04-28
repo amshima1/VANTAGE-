721d8c3d8eec15879b9e07275783e69279aab2aa
amshima1 <v.amshima9021@miva.edu.ng>
2026-04-28 23:50:26 +0100

    Update vendor.js

--- vendor.js
+++ vendor.js
@@ -1,1 +1,17 @@
+const db = firebase.firestore();
 
+function addProduct(){
+  let name = document.getElementById("name").value;
+  let price = document.getElementById("price").value;
+
+  let vendorId = "vendor_" + Math.floor(Math.random()*1000);
+
+  db.collection("products").add({
+    name,
+    price: Number(price),
+    vendorId,
+    sales: 0
+  });
+
+  alert("Product added");
+}
