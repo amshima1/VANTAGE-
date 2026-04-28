89505f0643a9577468e367f2139a39a65ec368b9
amshima1 <v.amshima9021@miva.edu.ng>
2026-04-28 23:53:29 +0100

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,24 @@
+const db = firebase.firestore();
 
+function loadOrders(){
+  db.collection("orders").onSnapshot(snapshot => {
+
+    let box = document.getElementById("orders");
+    box.innerHTML = "";
+
+    snapshot.forEach(doc => {
+      let o = doc.data();
+
+      box.innerHTML += `
+        <div>
+          <h3>Order</h3>
+          <p>Total: ₦${o.total}</p>
+          <p>Ref: ${o.ref}</p>
+        </div>
+      `;
+    });
+
+  });
+}
+
+loadOrders();
